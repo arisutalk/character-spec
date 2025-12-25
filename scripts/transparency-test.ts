3897dfcf716619f70738aba47a1ed8c255bfbe1d
concertypin <55056558+concertypin@users.noreply.github.com>
2025-12-25 23:39:05 +0900

    feat(build): Merge transparent schema types with JSDoc-rich types

--- scripts/transparency-test.ts
+++ scripts/transparency-test.ts
@@ -5,7 +5,7 @@ import { z } from "zod";
 // In its current "opaque" state (ZodType<T>), this should fail to compile if we try to access .shape
 // because ZodType doesn't have .shape. Only ZodObject has it.
 
-//@ts-expect-error - CharacterSchema is currently opaque (ZodType<Character>)
+// type CharacterShape = typeof CharacterSchema.shape;
 type CharacterShape = typeof CharacterSchema.shape;
 
 // If it were transparent, we could do something like this:
